39bbbcdd657ea08ed3da3aca27ce65ce011505c2
Christian Montesor <christianmontesor122@gmail.com>
2023-11-18 12:24:25 +0800

    LMS 8 Login, pictures

---
+++ login_page.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="book1">
+        <img src="images/books1.png">
+    </div>
+    <div class="logo">
+        <img src="images/logo2.png">
+        <h1>LOG IN</h1>
+        <h2>Email Address</h2>
+        <input type="text" class="email"><br><br>
+        <h2>Password</h2>
+        <input type="password" class="email">
+    </div>
+</div>
+</body>
+</html>
