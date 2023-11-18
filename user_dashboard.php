27a56c4f8f76a732f40a723c4d86447178907b43
jaderaposa <jaduya678@gmail.com>
2023-11-18 22:30:08 +0800

    asdasdasd

--- user_dashboard.php
+++ user_dashboard.php
@@ -11,22 +11,18 @@
 <body>
     <div class="container">
         <div class="logo">
-            <img src="images/logo2.png" width="90px" style="margin-top: 18px">
-            <li class="emlogo">LIBRARY MANAGEMENT SYSTEM</li>
+            <img src="images/logo2.png" width="90px">
+            <li>LIBRARY MANAGEMENT SYSTEM</li>
         </div>
-        <div class="navbar">
-            <ul>
-                <div>
-                    <li class="em">LIBRARY HOMAPAGE</li>
-                    <li class="em">DATABASES A-TO-Z</li>
-                    <li class="em">HELP</li>
-                </div>
-                <div>
-                    <li class="emnotif"><img src="images/bell-ring.png" width="30px" style="margin-top: 5px;"></li>
-                    <li class="email">07108487@dwc-legazpi.edu</li>
-                    <li class="next"><img src="images/next.png" width="20px"></li>
-                </div>
-            </ul>
+        <div style="display:flex;">
+            <li>LIBRARY HOMAPAGE</li>
+            <li>DATABASES A-TO-Z</li>
+            <li>HELP</li>
+        </div>
+        <div style="display:flex;align-items:center;">
+            <li><img src="images/bell-ring.png" width="30px" style="margin-top: 5px;"></li>
+            <li>07108487@dwc-legazpi.edu</li>
+            <li><img src="images/next.png" width="20px"></li>
         </div>
     </div>
 
