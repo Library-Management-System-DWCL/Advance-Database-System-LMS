7c8666e9edc0c7d7e5eda9316879bd429d0159e7
raven <ravenmusaarieta@gmail.com>
2023-11-18 22:06:20 +0800

    LMS-21 user_dasboard_navabar

---
+++ user_dashboard.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/user_dashboard.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+    <div class="header">
+            <div class="logo">
+                <img src="images/logo2.png" width="90px" style="margin-top: 18px">
+            </div>
+        <div class="navbar">
+            <ul>
+                <li class="emlogo">LIBRARY MANAGEMENT SYSTEM</li>
+                <li class="em">LIBRARY HOMAPAGE</li>
+                <li class="em">DATABASES A-TO-Z</li>
+                <li class="em">HELP</li>
+                <li class="emnotif"><img src="images/bell-ring.png" width="30px" style="margin-top: 5px;"></li>
+                <li class="email">07108487@dwc-legazpi.edu</li>
+                <li class="next"><img src="images/next.png" width="20px"></li>
+            </ul>
+        </div>
+    </div>
+    </div>
+    
+</body>
+</html>
