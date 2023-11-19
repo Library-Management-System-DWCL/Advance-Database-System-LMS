4f17c3ff120c4e6cbeb1a8e9adf87927eaf7ca15
raven <ravenmusaarieta@gmail.com>
2023-11-19 16:51:27 +0800

    nknkkjkjkjk

---
+++ admin_dashboard.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/admin_dashboard.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+        <div class="logo">
+            <div style="display: flex;align-items: center;">
+                <img src="images/logo2.png" width="90px">
+                <li>LIBRARY MANAGEMENT SYSTEM</li>
+            </div>
+        </div>
+        <div class="logo2">
+            <li>LIBRARY HOMEPAGE</li>
+            <li>DATABASES A-TO-Z</li>
+            <li>HELP</li>
+        </div>
+        <div style="display:flex;gap:1rem;align-items:center;margin-right: 40px;">
+            <li><img src="images/bell-ring.png" width="30px" style="margin-top: 5px;"></li>
+            <li><img src="images/user.png" width="30px"></li>
+            <li>ADMIN</li>
+            <li><img src="images/next.png" width="20px"></li>
+        </div>
+    </div>
+<div class="container2">
+    <div class="admin">
+            <div style="display: flex;align-items: center;">
+                <li>ADMIN DASHBOARD</li>
+            </div>
+        </div>
+        <div class="log">
+            <li>BOOK</li>
+            <li>CATEGORIES</li>
+            <li>CHANGED PASSWORD</li>
+        </div>
+</div>
+</body>
+</html>
