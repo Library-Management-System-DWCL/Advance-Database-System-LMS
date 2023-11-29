aadfe57644dc3830c41b498bdb0231e61b60be27
Ian-it-ai <07103944@dwc-legazpi.edu>
2023-11-29 17:09:01 +0800

    ian files

---
+++ Iancss.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css" />
+  
+    <title>Library Management System</title>
+    
+</head>
+<body>
+
+    <div id="">
+<div id="search-container1">
+    
+    <input type="text" id="search-bar" placeholder="Search...">
+    <button id="search-button">Search</button>
+</div>
+<nav>
+    <a href="#home">Home</a>
+    <a href="#catalog">Catalog</a>
+    <a href="#author">Author</a>
+    <a href="#popularity">Popularity</a>
+    <a href="#genres">Genres</a>
+    <a href="#recommendations">Recommendations</a>
+</nav>
+<div class=flex>
+<div class="box">
+    <h3>Library Hours</h3>
+    <p>11/20/2023 - MONDAY</p>
+    <p>7:30 AM - 5:00 PM</p>
+    <p>Contact:</p>
+    <p>09xxxxxxxxx</p>
+</div>
+
+<div id="quote-box">
+    <p id="quote">"When in doubt, always go to the library."</p>
+    <div id="search-container">
+        <!-- Search bar -->
+        <input type="text" id="search-bar" placeholder="Search...">
+        <button id="search-button">Search</button>
+    </div>
+</div>
+
+<div id="news-box">
+    <h3>News</h3>
+    <p>- New Science Books Arrived</p>
+    <p>- Library is Closed on Wednesday</p>
+    <p>- Upcoming Event</p>
+</div>
+</div>
+<div class=flex>
+
+<div class="con" id="finds-box">
+    <h3>Finds</h3>
+    <p>- Articles</p>
+    <p>- Books</p>
+    <p>- Journals</p>
+</div>
+
+<div class="nin" id="about-box">
+    <h3>About</h3>
+    <p>Introducing the Library Management System, your complete solution for effective book organizing and management. Whether you're an individual book lover or a member of a library or other organization, our platform is made to make the process of classifying, organizing, and accessing your book collection as simple as possible.</p>
+    <p>Become a member of Library Management System right now to start managing your library. We can help you if you're passionate about reading trying to arrange your own collection.</p>
+</div>
+
+<div class="non" id="help-box">
+    <h3>Help</h3>
+    <p>- Ask Librarians</p>
+    <p>- Contacts</p>
+    <p>- FAQs</p>
+    <p>- Faculty Services</p>
+</div>
+</div>
